2beab4b508b73adf87eb0c87fd7ce3c46d5d83f3
Pichunov-N <npichunov@gmail.com>
2022-02-10 21:57:22 +0200

    completed two tasks of fourth hw

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+//1
+// const searchString = (a, b) => console.log(a.toLowerCase().includes(b));
+
+// searchString('originalStr', 'str')
+
+
+//2
+// const capitalizeFirstLetter = (a) => {
+//     let result = a[0].toUpperCase() + a.slice(1);
+//     return result;
+// }
+
+// console.log(capitalizeFirstLetter("hELLO"));
